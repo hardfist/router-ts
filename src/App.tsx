5b09b6a5265733a903ecfef61307ccf579edffaa
hardfist <1562502418@qq.com>
2019-06-04 00:16:31 +0800

    add lazy
    Change-Id: I25f945e732a0b5f1a2210346566d38211c15fd9e

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import { Router, Link, Route}  from './route';
+import React from "react";
+import { Router, Link, Route } from "./route";
+const Lazy = React.lazy(() => import("./card"));
 
 const App: React.FC = () => {
   return (
     <>
-    hello
-    <Router>
-      <Link href="/path" >path</Link>
-      <Link href="/entry">entry</Link>
-      <Route path="/path">this is a path </Route>
-      <Route path="/entry">this is a entry</Route>
-    </Router>
+      <React.Suspense
+        fallback={() => {
+          return <div>loading....</div>;
+        }}
+      >
+        <Router>
+          <Link href="/path">path</Link>
+          <Link href="/entry">entry</Link>
+          <Link href="/lazy">lazy</Link>
+          <Route path="/path">this is a path </Route>
+          <Route path="/entry">this is a entry</Route>
+          <Route path="/lazy">
+            <Lazy />
+          </Route>
+        </Router>
+      </React.Suspense>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/Lazy.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Lazy extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Lazy;
